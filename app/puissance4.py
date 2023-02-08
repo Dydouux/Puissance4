9499e53104dcebd19fccfaddd77d4dd65473cc70
Dydoux <dylan.roumegoux@e.rascol.net>
2023-02-08 15:39:04 +0100

    second version

--- app/puissance4.py
+++ app/puissance4.py
@@ -1,152 +1,62 @@
-import numpy as np
-import pygame
-import sys
-import math
- 
-BLUE = (0,0,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-YELLOW = (255,255,0)
- 
-ROW_COUNT = 6
-COLUMN_COUNT = 7
- 
-def create_board():
-    board = np.zeros((ROW_COUNT,COLUMN_COUNT))
-    return board
- 
-def drop_piece(board, row, col, piece):
-    board[row][col] = piece
- 
-def is_valid_location(board, col):
-    return board[ROW_COUNT-1][col] == 0
- 
-def get_next_open_row(board, col):
-    for r in range(ROW_COUNT):
-        if board[r][col] == 0:
-            return r
- 
-def print_board(board):
-    print(np.flip(board, 0))
- 
-def winning_move(board, piece):
-    # Check horizontal locations for win
-    for c in range(COLUMN_COUNT-3):
-        for r in range(ROW_COUNT):
-            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
-                return True
- 
-    # Check vertical locations for win
-    for c in range(COLUMN_COUNT):
-        for r in range(ROW_COUNT-3):
-            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
-                return True
- 
-    # Check positively sloped diaganols
-    for c in range(COLUMN_COUNT-3):
-        for r in range(ROW_COUNT-3):
-            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
-                return True
- 
-    # Check negatively sloped diaganols
-    for c in range(COLUMN_COUNT-3):
-        for r in range(3, ROW_COUNT):
-            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
-                return True
- 
-def draw_board(board):
-    for c in range(COLUMN_COUNT):
-        for r in range(ROW_COUNT):
-            pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
-            pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
-     
-    for c in range(COLUMN_COUNT):
-        for r in range(ROW_COUNT):      
-            if board[r][c] == 1:
-                pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-            elif board[r][c] == 2: 
-                pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-    pygame.display.update()
- 
- 
-board = create_board()
-print_board(board)
-game_over = False
-turn = 0
- 
-#initalize pygame
-pygame.init()
- 
-#define our screen size
-SQUARESIZE = 100
- 
-#define width and height of board
-width = COLUMN_COUNT * SQUARESIZE
-height = (ROW_COUNT+1) * SQUARESIZE
- 
-size = (width, height)
- 
-RADIUS = int(SQUARESIZE/2 - 5)
- 
-screen = pygame.display.set_mode(size)
-#Calling function draw_board again
-draw_board(board)
-pygame.display.update()
- 
-myfont = pygame.font.SysFont("monospace", 75)
- 
-while not game_over:
- 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
- 
-        if event.type == pygame.MOUSEMOTION:
-            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-            posx = event.pos[0]
-            if turn == 0:
-                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
-            else: 
-                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
-        pygame.display.update()
- 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-            #print(event.pos)
-            # Ask for Player 1 Input
-            if turn == 0:
-                posx = event.pos[0]
-                col = int(math.floor(posx/SQUARESIZE))
- 
-                if is_valid_location(board, col):
-                    row = get_next_open_row(board, col)
-                    drop_piece(board, row, col, 1)
- 
-                    if winning_move(board, 1):
-                        label = myfont.render("Player 1 wins!!", 1, RED)
-                        screen.blit(label, (40,10))
-                        game_over = True
- 
- 
-            # # Ask for Player 2 Input
-            else:               
-                posx = event.pos[0]
-                col = int(math.floor(posx/SQUARESIZE))
- 
-                if is_valid_location(board, col):
-                    row = get_next_open_row(board, col)
-                    drop_piece(board, row, col, 2)
- 
-                    if winning_move(board, 2):
-                        label = myfont.render("Player 2 wins!!", 1, YELLOW)
-                        screen.blit(label, (40,10))
-                        game_over = True
- 
-            print_board(board)
-            draw_board(board)
- 
-            turn += 1
-            turn = turn % 2
- 
-            if game_over:
-                pygame.time.wait(3000)
+import tkinter as tk
+
+class PowerFourGUI:
+    def __init__(self, master):
+        self.master = master
+        self.grid = [[0 for x in range(7)] for y in range(6)]
+        self.player = 1
+        self.buttons = []
+        for row in range(6):
+            button_row = []
+            for col in range(7):
+                button = tk.Button(master, text=" ", command=lambda row=row, col=col: self.play(row, col))
+                button.grid(row=row, column=col)
+                button_row.append(button)
+            self.buttons.append(button_row)
+
+    def play(self, row, col):
+        for i in range(6):
+            if self.grid[5 - i][col] == 0:
+                self.grid[5 - i][col] = self.player
+                if self.player == 1:
+                    color = "red"
+                else:
+                    color = "blue"
+                self.buttons[5 - i][col].config(bg=color)
+                break
+
+        if self.check_winner():
+            tk.messagebox.showinfo("Winner", "Player {} wins!".format(self.player))
+            self.master.quit()
+
+        if all(all(val != 0 for val in row) for row in self.grid):
+            tk.messagebox.showinfo("Draw", "Draw!")
+            self.master.quit()
+
+        self.player = 3 - self.player
+
+    def check_winner(self):
+        # check rows
+        for row in self.grid:
+            for i in range(4):
+                if row[i:i + 4] == [self.player, self.player, self.player, self.player]:
+                    return True
+
+        # check columns
+        for col in range(7):
+            for i in range(3):
+                if [self.grid[i][col], self.grid[i + 1][col], self.grid[i + 2][col], self.grid[i + 3][col]] == [self.player, self.player, self.player, self.player]:
+                    return True
+
+        # check diagonals
+        for row in range(3):
+            for col in range(4):
+                if [self.grid[row][col], self.grid[row + 1][col + 1], self.grid[row + 2][col + 2], self.grid[row + 3][col + 3]] == [self.player, self.player, self.player, self.player]:
+                    return True
+                if [self.grid[row][col + 3], self.grid[row + 1][col + 2], self.grid[row + 2][col + 1], self.grid[row + 3][col]] == [self.player, self.player, self.player, self.player]:
+                    return True
+        return False
+
+root = tk.Tk()
+game = PowerFourGUI(root)
+root.mainloop()
